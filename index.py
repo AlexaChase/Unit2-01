2d071861df18d46df72f803e85b00b5e35f552eb
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-03-01 15:36:36 +0000

    new changes

--- index.py
+++ index.py
@@ -1,3 +1,15 @@
 print("Hello, World!")
 username = input("What is your name?")
 print("Nice to meet you,", username)
+import random
+secretNumber = random.randint(1, 100)
+print("I'm thinking of a number between 1 and 10...")
+while True:
+    guess = int(input("What is your guess? "))
+    if (guess == secretNumber):
+        break
+    if (guess > secretNumber):
+        print("Sorry, your guess is too high.")
+    if (guess < secretNumber):
+        print("Sorry, your guess is too low.")
+print("You got it!")
